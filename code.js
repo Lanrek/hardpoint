c24d75fd51415070dbad632e158796863d5a432c
Lanrek <Lanrek@users.noreply.github.com>
2018-04-08 02:54:49 -0400

    Initial prototype!

---
+++ code.js
@@ -0,0 +1,663 @@
+class ShipId {
+	constructor(specificationId, modificationId) {
+		this.specificationId = specificationId;
+		this.modificationId = modificationId;
+		
+		// This is crazy -- must be missing something about how loadouts are bound to ships.
+		var loadoutId = undefined;
+		var tryLoadoutId = function(id) {
+			if (!loadoutId) {
+				if (shipLoadouts[id]) {
+					loadoutId = id;
+				}
+			}
+		}
+		
+		var specificationData = shipSpecifications[this.specificationId]._data;
+		if (this.modificationId) {
+			tryLoadoutId(specificationId + "_" + this.modificationId);
+			
+			var patchFile = specificationData["Modifications"][this.modificationId]["@patchFile"];
+			if (patchFile) {
+				var fileName = patchFile.split("/").pop();
+				tryLoadoutId(fileName + "_" + this.modificationId);
+				tryLoadoutId(fileName);
+			}
+		}
+		else
+		{
+			tryLoadoutId(specificationId);
+			if (specificationData["@local"]) {
+				// The worst hack of this mess, but necessary for the Constellation Andromeda.
+				tryLoadoutId(specificationData["@local"].replace(/ /g, "_"));
+			}
+		}
+		
+		this.loadoutId = loadoutId;
+	}
+}
+
+class ShipSpecification {
+	constructor(data) {
+		this._data = data;
+		
+		this._itemPorts = this._findItemPorts(undefined);
+		this._modificationItemPorts = {};
+		this.modificationIds.forEach(n => this._modificationItemPorts[n] = this._findItemPorts(n));
+	}
+	
+	get tags() {
+		if (this._data["@itemPortTags"]) {
+			return this._data["@itemPortTags"].split(" ");
+		}
+		
+		return [];
+	}
+	
+	get modificationIds() {
+		return Object.keys(this._data["Modifications"] || {});
+	}
+	
+	getItemPorts(modificationId) {
+		if (modificationId) {
+			return this._modificationItemPorts[modificationId];
+		}
+		
+		return this._itemPorts;
+	}
+	
+	getModificationLoadouts() {
+		var modifications = [undefined].concat(this.modificationIds);
+		var results = modifications.map(x => new ShipId(this._data["@name"], x));
+		
+		// Filter out anything that doesn't have a valid loadout.
+		results = results.filter(n => n.loadoutId);
+		
+		return results;
+	}
+	
+	getAttributes(modificationId) {
+		return [
+			{
+				name: "Normal Speed",
+				category: "Maneuverability",
+				value: this._data["ifcs"]["@SCMVelocity"]
+			},
+			{
+				name: "Afterburner Speed",
+				category: "Maneuverability",
+				value: this._data["ifcs"]["@ABSCM"]
+			},
+			{
+				name: "Cruise Speed",
+				category: "Maneuverability",
+				value: this._data["ifcs"]["@CruiseSpeed"]
+			},
+			{
+				name: "Total Hitpoints",
+				category: "Survivability",
+				value: this._findParts(modificationId).reduce((total, x) => total + Number(x["@damageMax"] || 0), 0)
+			}
+		];
+	}
+	
+	_findParts(modificationId, className = undefined) {
+		var unsearched = [this._data["Parts"]["Part"]];
+		if (modificationId) {
+			var modification = this._data["Modifications"][modificationId];
+			if (modification["mod"] && modification["mod"]["Parts"]) {
+				unsearched = [modification["mod"]["Parts"]["Part"]];
+			}
+		}
+		
+		var result = [];
+		while (unsearched.length > 0) {
+			var potential = unsearched.pop();
+			
+			if (!className || potential["@class"] == className) {
+				result.push(potential);
+			}
+			
+			if (potential["Parts"] && potential["Parts"]["Part"])
+			{
+				if (Array.isArray(potential["Parts"]["Part"])) {
+					potential["Parts"]["Part"].forEach(n => unsearched.push(n));
+				}
+				else {
+					unsearched.push(potential["Parts"]["Part"]);
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	_findItemPorts(modificationId = undefined) {
+		var itemPorts = this._findParts(modificationId, "ItemPort");
+		
+		return itemPorts.filter(n => n["ItemPort"]).map(function (part) {
+			return new SpaceshipItemPort(part["ItemPort"], part["@name"]);
+		});
+	}
+}
+
+class SpaceshipComponent {
+	constructor(data) {
+		this._data = data;
+		
+		this.itemPorts = this._findItemPorts();
+	}
+	
+	get name() {
+		return this._data["@name"];
+	}
+	
+	get type() {
+		return this._data["@itemType"];
+	}
+	
+	get subtype() {
+		return this._data["@itemSubType"];
+	}
+	
+	get size() {
+		return this._data["@itemSize"];
+	}
+	
+	get requiredTags() {
+		if (!this._data["@requiredPortTags"]) {
+			return [];
+		}
+		
+		return this._data["@requiredPortTags"].split(" ");
+	}
+	
+	_findItemPorts() {
+		if (!this._data["ports"]) {
+			return [];
+		}
+
+		return this._data["ports"].map(port => new SpaceshipItemPort(port, port["@name"]));
+	}
+}
+
+class DataforgeComponent {
+	constructor(data) {
+		this._data = data;
+		
+		this.itemPorts = this._findItemPorts();
+	}
+	
+	get name() {
+		return this._data["@name"];
+	}
+	
+	get type() {
+		return this._data["@type"];
+	}
+	
+	get subtype() {
+		return this._data["Components"]["SCItem"]["ItemDefinition"]["@SubType"];
+	}
+	
+	get size() {
+		return this._data["@size"];
+	}
+	
+	get requiredTags() {
+		var tags = this._data["Components"]["SCItem"]["ItemDefinition"]["@RequiredTags"];
+		if (!tags) {
+			return [];
+		}
+		
+		return tags.split(" ");
+	}
+	
+	get totalShields() {
+		var params = this._data["Components"]["SCItemShieldGeneratorParams"];
+		if (params) {
+			return params["ShieldEmitterContributions"]["@MaxShieldHealth"];
+		}
+	}
+	
+	_findItemPorts() {
+		var ports = this._data["Components"]["SCItem"]["ItemPorts"];
+		if (!ports) {
+			return [];
+		}
+		
+		var ports = ports["SItemPortCoreParams"]["Ports"];
+		if (!ports) {
+			return [];
+		}
+		
+		ports = ports["SItemPortDef"];
+		if (!Array.isArray(ports)) {
+			ports = [ports];
+		}
+		
+		return ports.map(port => new DataforgeItemPort(port));
+	}
+}
+
+class ItemPortType {
+	constructor(portType) {
+		this.type = portType["@type"];
+	
+		// It looks like there are implicit defaults; the Sabre is an example.
+		var subTypes = portType["@subtypes"];
+		if (!subTypes) {
+			if (this.type == "WeaponGun") {
+				subTypes = "Gun";
+			}
+			else if (this.type == "Turret") {
+				subTypes = "GunTurret";
+			}
+			else {
+				subTypes = "Default";
+			}
+		}
+		
+		this.subtypes = undefined;
+		if (subTypes) {
+			this.subtypes = subTypes.split(",");
+		}
+	}
+}
+
+class BaseItemPort {
+	availableComponents(tags) {
+		return Object.keys(mergedComponents).filter(n => this.matchesComponent(tags, mergedComponents[n]));
+	}
+	
+	matchesType(type, subtype) {
+		if (!this.types) {
+			return false;
+		}
+		
+		return this.types.some(function (portType) {
+			if (portType.type != type) {
+				return false;
+			}
+			
+			// Match all subtypes when looking for interesting types.
+			if (subtype != undefined) {
+				if (!portType.subtypes.some(s => s == subtype)) {
+					return false;
+				}
+			}
+			
+			return true;
+		});
+	}
+	
+	matchesComponent(tags, component) {
+		if (!this.matchesType(component.type, component.subtype)) {
+			return false;
+		}
+		
+		if (this.minSize != undefined && component.size < this.minSize) {
+			return false;
+		}
+		
+		if (this.maxSize != undefined && component.size > this.maxSize) {
+			return false;
+		}
+		
+		if (component.requiredTags) {
+			var portTags = tags.concat(this.tags);
+			
+			for (var tag of component.requiredTags) {
+				if (!portTags.includes(tag)) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+}
+
+class SpaceshipItemPort extends BaseItemPort {
+	constructor(data, name) {
+		super();
+		
+		this._data = data;
+		
+		this.name = name;
+		this.editable = !(data["@flags"] || "").includes("uneditable");
+	}
+	
+	get minSize() {
+		return this._data['@minsize'];
+	}
+	
+	get maxSize() {
+		return this._data['@maxsize'];
+	}
+	
+	get tags() {
+		if (this._data["@portTags"]) {
+			return this._data["@portTags"].split(" ");
+		}
+		
+		return [];
+	}
+	
+	get types() {
+		if (this._data == undefined || this._data["Types"] == undefined) {
+			return [];
+		}
+		
+		var types = this._data["Types"]["Type"];
+		if (!Array.isArray(types)) {
+			types = [types];
+		}
+		
+		return types.map(function (type) {
+			return new ItemPortType(type)
+		});
+	}
+}
+
+class DataforgeItemPort extends BaseItemPort {
+	constructor(data) {
+		super();
+		
+		this._data = data;
+		
+		this.name = data["@Name"];
+		this.editable = !(data["@Flags"] || "").includes("uneditable");
+	}
+	
+	get minSize() {
+		return this._data['@MinSize'];
+	}
+	
+	get maxSize() {
+		return this._data['@MaxSize'];
+	}
+	
+	get tags() {
+		if (this._data["@PortTags"]) {
+			return this._data["@PortTags"].split(" ");
+		}
+		
+		return [];
+	}
+	
+	get types() {
+		var subtypes = this._data["Types"]["SItemPortDefTypes"]["@Type"]["SubTypes"];
+		if (subtypes) {
+			var subtype = subtypes["Enum"]["@value"];
+		}
+		var type = new ItemPortType(
+			this._data["Types"]["SItemPortDefTypes"]["@Type"],
+			subtype);
+		return [type];
+	}
+}
+
+class ShipCustomization {
+	constructor(shipId) {
+		this.shipId = shipId;
+		this._overrides = {};
+	}
+	
+	// Abstracted because ship modifications will probably have their own tags eventually.
+	get tags() {
+		return this._specification.tags;
+	}
+	
+	getAttributes(category=undefined) {
+		var attributes = this._specification.getAttributes(this.shipId.modificationId);
+		
+		attributes = attributes.concat([
+			{
+				name: "Total Shields",
+				category: "Survivability",
+				value: this._getDirectlyAttachedComponents().reduce((total, x) => total + Number(x.totalShields || 0), 0)
+			}
+		]);
+		
+		if (category) {
+			attributes = attributes.filter(n => n.category == category);
+		}
+		return attributes;
+	}
+	
+	getItemPortsMatchingTypes(types) {
+		return this._specification.getItemPorts(this.shipId.modificationId).filter(function (port) {
+			return types.some(n => port.matchesType(n, undefined));
+		});
+	}
+	
+	setAttachedComponent(portName, parentPortName, componentName) {
+		var overrideName = this._makeOverrideName(portName, parentPortName);
+		Vue.set(this._overrides, overrideName, componentName);
+	}
+	
+	getAttachedComponent(portName, parentPortName) {
+		var componentName;
+		var overrideName = this._makeOverrideName(portName, parentPortName);
+		
+		if (overrideName in this._overrides) {
+			componentName = this._overrides[overrideName] ;
+		}
+		else {
+			if (parentPortName) {
+				var entry = this._loadout.find(n => n[parentPortName] != undefined);
+				if (!entry) {
+					return undefined;
+				}
+				
+				var attached = entry["attached"];
+				if (!attached) {
+					return undefined;
+				}
+				
+				var childEntry = attached.find(n => n[portName] != undefined);
+				if (!childEntry) {
+					return undefined;
+				}
+				componentName = childEntry[portName];
+			}
+			else {
+				var entry = this._loadout.find(n => n[portName] != undefined);
+				if (!entry) {
+					return undefined;
+				}
+				
+				componentName = entry[portName];
+			}
+		}
+
+		return mergedComponents[componentName];
+	}
+
+	get _specification() {
+		return shipSpecifications[this.shipId.specificationId];
+	}
+
+	get _loadout() {
+		return shipLoadouts[this.shipId.loadoutId];
+	}
+	
+	_getDirectlyAttachedComponents() {
+		var attached = [];
+		var directPorts = this._specification.getItemPorts(this.shipId.modificationId);
+		directPorts.forEach(x => attached.push(this.getAttachedComponent(x.name)));
+		return attached.filter(n => n);
+	}
+	
+	_makeOverrideName(portName, parentPortName) {
+		if (parentPortName) {
+			return parentPortName + "." + portName;
+		}
+		
+		return portName;
+	}
+}
+
+var componentDisplay = Vue.component('component-display', {
+	template: '#component-display',
+	props: ['componentName', 'disabled'],
+	data: function() {
+		return {
+		}
+	},
+	computed: {
+		component: function () {
+			return mergedComponents[this.componentName];
+		}
+	},
+	watch: {
+	},
+	methods: {
+	}
+});
+
+var componentSelector = Vue.component('component-selector', {
+	template: '#component-selector',
+	props: ['customization', 'itemPort', 'parentPortName'],
+	data: function() {
+		return {
+			selectedComponentName: this.getSelectedComponentName(),
+			visible: false
+		}
+	},
+	computed: {
+		availableComponents: function () {
+			return this.itemPort.availableComponents(this.customization.tags);
+		},
+		label: function () {
+			var size = this.itemPort.minSize;
+			if (this.itemPort.minSize != this.itemPort.maxSize)
+			{
+				size += "-" + this.itemPort.maxSize;
+			}
+			return this.itemPort.name + " (size " + size + ")";
+		}
+	},
+	watch: {
+		selectedComponentName: function (val) {
+			this.customization.setAttachedComponent(this.itemPort.name, this.parentPortName, val);
+		},
+		
+		// Invalidate the selected component when the properties change from widget reuse.
+		// TODO Not sure why this is necessary...
+		customization: function(val) {
+			this.selectedComponentName = this.getSelectedComponentName();
+		},
+		itemPort: function(val) {
+			this.selectedComponentName = this.getSelectedComponentName();
+		},
+		parentPortName: function(val) {
+			this.selectedComponentName = this.getSelectedComponentName();
+		}
+	},
+	methods: {
+		onVisibleChange: function(visible) {
+			this.visible = visible;
+			
+			// Fix the width of the dropdown dynamically since the dropdown widget doesn't handle padding.
+			var elementWidth = this.$refs["dropdown"].$el.offsetWidth;
+			this.$refs["dropdown"].$refs["drop"].$el.style.width = elementWidth + "px";
+		},
+		onClick: function(name) {
+			this.selectedComponentName = name;
+		},
+		getSelectedComponentName: function () {
+			var component = this.customization.getAttachedComponent(this.itemPort.name, this.parentPortName);
+			if (component) {
+				return component.name;
+			}
+		},
+	}
+});
+
+
+// Translate underlying data into model objects.
+for (key of Object.keys(shipSpecifications)) {
+	shipSpecifications[key] = new ShipSpecification(shipSpecifications[key]);
+}
+
+var mergedComponents = {}
+for (key of Object.keys(dataforgeComponents)) {
+	mergedComponents[key] = new DataforgeComponent(dataforgeComponents[key]);
+}
+for (key of Object.keys(spaceshipComponents)) {
+	if (!(key in mergedComponents)) {
+		mergedComponents[key] = new SpaceshipComponent(spaceshipComponents[key]);
+	}
+}
+
+
+var app = new Vue({
+	el: '#app',
+	data: {
+		selectedShipIdIndex: undefined,
+		selectedCustomization: {},
+
+		expandedSections: ["Weapons"],
+		sections: {
+			"Weapons": ['WeaponMissile', 'WeaponGun', 'Turret', 'TurretBase'],
+			"Systems": ['Cooler', 'Shield', 'PowerPlant']
+		},
+		
+		maneuverabilityColumns: [
+			{
+				title: "Maneuverability",
+				key: "name"
+			},
+			{
+				title: " ",
+				key: "value",
+				width: 100
+			}
+		],
+		survivabilityColumns: [
+			{
+				title: "Survivability",
+				key: "name"
+			},
+			{
+				title: " ",
+				key: "value",
+				width: 100
+			}
+		],
+	},
+	watch: {
+		selectedShipIdIndex: function (val) {
+			var shipId = this.shipIds[val];
+			
+			this.selectedCustomization = new ShipCustomization(shipId);
+		}
+	},
+	computed: {
+		shipIds: function () {
+			var unflattened = Object.values(shipSpecifications).map(n => n.getModificationLoadouts());
+			var flattened = unflattened.reduce((total, n) => total.concat(n), []);
+			
+			var npcModifications = ["Pirate", "S42", "SQ42", "Dead", "CalMason", "Weak"]
+			var filtered = flattened.filter(x => !x.modificationId || !npcModifications.some(n => x.modificationId.includes(n)));
+			
+			return filtered;
+		}
+	},
+	methods: {
+		getAttachedComponentName: function (portName, childPortName = undefined) {
+			var component = this.selectedCustomization.getAttachedComponent(portName, childPortName);
+			if (component) {
+				return component.name;
+			}
+		},
+		getAttachedComponentItemPorts: function (portName) {
+			var component = this.selectedCustomization.getAttachedComponent(portName);
+			if (component) {
+				return component.itemPorts;
+			}
+		}
+	}
+});
---
+++ scripts/download.py
@@ -0,0 +1,98 @@
+from html.parser import HTMLParser
+from urllib.request import urlopen
+import os
+import re
+
+
+re_url = re.compile(r'^(([a-zA-Z_-]+)://([^/]+))(/.*)?$')
+
+def resolve_link(link, url):
+    m = re_url.match(link)
+    if m is not None:
+        if not m.group(4):
+            # http://domain -> http://domain/
+            return link + '/'
+        else:
+            return link
+    elif link[0] == '/':
+        # /some/path
+        murl = re_url.match(url)
+        return murl.group(1) + link
+    else:
+        # relative/path
+        if url[-1] == '/':
+            return url + link
+        else:
+            return url + '/' + link
+
+
+class ListingParser(HTMLParser):
+    """Parses an HTML file and build a list of links.
+    Links are stored into the 'links' set. They are resolved into absolute
+    links.
+    """
+    def __init__(self, url):
+        HTMLParser.__init__(self)
+
+        if url[-1] != '/':
+            url += '/'
+        self.__url = url
+        self.links = set()
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            for key, value in attrs:
+                if key == 'href':
+                    if not value:
+                        continue
+                    value = resolve_link(value, self.__url)
+                    self.links.add(value)
+                    break
+
+
+def download_directory(url, target):
+    def mkdir():
+        if not mkdir.done:
+            try:
+                os.mkdir(target)
+            except OSError:
+                pass
+            mkdir.done = True
+    mkdir.done = False
+
+    response = urlopen(url)
+
+    if response.info().get_content_type() == 'text/html':
+        contents = response.read()
+
+        parser = ListingParser(url)
+        parser.feed(contents.decode("utf-8"))
+        for link in parser.links:
+            link = resolve_link(link, url)
+            if link[-1] == '/':
+                link = link[:-1]
+            if not link.startswith(url):
+                continue
+            name = link.rsplit('/', 1)[1]
+            if '?' in name:
+                continue
+            mkdir()
+            download_directory(link, os.path.join(target, name))
+        if not mkdir.done:
+            # We didn't find anything to write inside this directory
+            # Maybe it's a HTML file?
+            if url[-1] != '/':
+                with open(target, 'wb') as fp:
+                    fp.write(contents)
+    else:
+        buffer_size = 4096
+        with open(target, 'wb') as fp:
+            chunk = response.read(buffer_size)
+            while chunk:
+                fp.write(chunk)
+                chunk = response.read(buffer_size)
+
+
+snapshot_name = "3.1.0-739258"
+snapshot_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data", snapshot_name))
+download_directory("http://starcitizendb.com/api/", snapshot_path)
